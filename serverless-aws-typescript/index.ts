df6fb2f96a337f8c3f2b4d64cf31111c09e7305f
ty44 <y.takahashi424@gmail.com>
2024-02-08 21:36:52 +0900

    📦 feat(index.ts): add ulid package to generate unique identifiers 🔧 refactor(index.ts): replace single date response with an object containing cachedDate, date, and id 🔧 refactor(index.html): update HTML to display both current and cached time 🎨 style(index.html): change heading from 'Serverless with Pulumi' to 'Hello world' for simplicity

--- serverless-aws-typescript/index.ts
+++ serverless-aws-typescript/index.ts
@@ -1,13 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as apigateway from "@pulumi/aws-apigateway";
+import { ulid } from 'ulid';
+
+// global variable
+const DATE = new Date().toUTCString();
 
 // A Lambda function to invoke
 const fn = new aws.lambda.CallbackFunction("fn", {
     callback: async (ev, ctx) => {
         return {
             statusCode: 200,
-            body: new Date().toISOString(),
+            body: JSON.stringify({
+                cachedDate: DATE,
+                date: new Date().toUTCString(),
+                id: ulid()
+            }),
         };
     }
 })
